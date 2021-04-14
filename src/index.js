2f81e3a697760d0dd0eb827596db68e49b7cb563
Erik Borg <e.borg1990@gmail.com>
2021-04-14 23:58:22 +0300

    -

--- src/index.js
+++ src/index.js
@@ -20,3 +20,12 @@ function App() {
 const root = document.querySelector("#root");
 
 render(<App/>, root);
+
+function App(props) {
+    const watch = <button>Watch</button>
+    const rate = <button>Rate</button>
+    if (props.seen) {
+        return <div className="container">{watch}</div>
+    }
+    return <div className="container">{rate}</div>
+}
